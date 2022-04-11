81c2b627ff6e6f537be25cb6d2e0da94fa2f6baf
Njoki <kinuthiash@gmail.com>
2022-04-11 11:56:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const people = ["Guadalupe", "Ollie" , "Aki"]
+const eventFunction = "surprise";
+// for loop 
+function writeCards(people, event) {
+  let thankyouList = [];
+  for (let i = 0; i < people.length; i++) {
+    let thanksMessage = `Thank you, ${people[i]}, for the wonderful ${event} gift!`;
+    thankyouList.push(thanksMessage)
+  }
+  return thankyouList
+}
+console.log(writeCards(people,eventFunction))
+
+// while loop
+function countDown(positiveInt){
+  let i = positiveInt;
+  while(i>=0){
+    console.log(i)
+    i--;
+  }  
+}
+
+console.log(countDown(10))
